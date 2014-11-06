2980b043a6af27fb90ad0f2fdc0f81994b38006d
jmjvvv <jmjvvv@gmail.com>
2014-11-06 15:16:25 +0900

    d

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function draw() {
    var timing2 = (new Date()%duration2)/duration2;
    var timing3 = (new Date()%duration3)/duration3;
 
-   fill(0); //우주선
+    //우주선
                                   // height
    image(img,600 + Math.cos(timing1*2*PI)*500,          
         400 + Math.sin(timing1*2*PI)*0);
